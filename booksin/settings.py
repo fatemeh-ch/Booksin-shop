d21827f7bcf2ab8434edf8b322c9e999a19b61cd
Fatemeh <fatemeh.ch.official@gmail.com>
2025-12-21 23:00:45 +0330

    MEDIA_URL was added to settings.py.

--- booksin/settings.py
+++ booksin/settings.py
@@ -122,6 +122,8 @@ STATIC_URL = 'statics/'
 
 STATICFILES_DIRS = [BASE_DIR/'statics']
 
+MEDIA_URL='/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
