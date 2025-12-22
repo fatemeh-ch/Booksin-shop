43fb4ca43aa918b93309a8d3e03d3e504828813e
Fatemeh <fatemeh.ch.official@gmail.com>
2025-12-22 23:02:44 +0330

    Contact.html was added to urlpatterns in app -core-.

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index_view, name='home')
+    path('', index_view, name='home'),
+    path('contact',contact_view, name='contact')
 ]
 
 app_name='core'
