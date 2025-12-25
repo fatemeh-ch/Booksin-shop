dfbddbd627b03b1f97e36d9132d83d00aa48f743
Fatemeh <fatemeh.ch.official@gmail.com>
2025-12-25 23:23:36 +0330

    About_url was created in urls.py (app-core-).

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', index_view, name='home'),
-    path('contact/',contact_view, name='contact')
+    path('contact/',contact_view, name='contact'),
+    path('about/',about_view,name='about')
 ]
 
 app_name='core'
