47ae4ff3826efaa449791ebf56eab9b41d81fe25
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-01 20:29:59 +0330

    Class Meta was added to model Post and its migrations were done in models.py -blog-.

--- blog/models.py
+++ blog/models.py
@@ -22,3 +22,6 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True)
+
+    class Meta:
+        ordering = ['-created_date']
