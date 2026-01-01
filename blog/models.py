f94bc8a9a55c5abe6fa7bad00fe1b1062665c29f
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-01 20:25:33 +0330

    Class category was created in models.py -blog-.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Class of categories
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 # Class of posts
 class Post(models.Model):
