989c6dc1832361a806ddecc6f04c94c573368572
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-01 20:15:11 +0330

    Post model was created in models.py-blog-.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+# Class of posts
+class Post():
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # category
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
