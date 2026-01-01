df6f60b5406105e054b0a44fe4dc61aced5d3cf3
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-01 21:01:07 +0330

    View of blog_home was editted and context & an object of Model Post were added to it for showing post details from database.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
+
 def blog_home_view(request):
-    return render(request,'blog/blog_home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog_home.html', context)
