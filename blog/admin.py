28fccab75ef6419ae08fae0def5d0b4b8c8083c6
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-01 20:38:58 +0330

    Class PostAdmin was created in admin.py -blog- for managin Post model.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-Empty'
+    list_display = ['title', 'author', 'status', 'created_date']
+    fields = ['title', 'content', 'status', 'author',
+              'counted_views', 'image', 'category', 'published_date']
+    list_filter = ('status', 'author')
+    ordering = ('-created_date',)
+    search_fields = ['title']
