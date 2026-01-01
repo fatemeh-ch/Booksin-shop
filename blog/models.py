34a30f071bea642479c96f5b1b76150f075a527d
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-01 20:26:27 +0330

    Category field was added to model Post in models.py -blog-.

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # category
+    category=models.ManyToManyField(Category)
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=0)
