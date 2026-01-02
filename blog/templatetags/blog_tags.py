af7d84d9c54f13cc9437cde89296e8b04ee4e5ab
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-02 21:42:28 +0330

    Latest_posts function was created in blog_tags.py for showing 3 last posts.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,6 +3,7 @@ from blog.models import Post, Category
 
 register = template.Library()
 
+
 @register.inclusion_tag('blog/post-categories.html')
 def post_categories():
     posts = Post.objects.filter(status=1)
@@ -11,3 +12,9 @@ def post_categories():
     for name in categories:
         cat_dict[name] = posts.filter(category=name).count()
     return {'categories': cat_dict}
+
+
+@register.inclusion_tag('blog/latestposts.html')
+def latest_posts():
+    posts = Post.objects.filter(status=1).order_by('-published_date')[:3]
+    return {'posts': posts}
