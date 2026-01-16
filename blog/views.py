39152f7eae16e0e94b469177da9287d8a80e1f18
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-16 16:50:00 +0330

    Search view was created in views.py (blog).

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ def blog_home_view(request,cat_name=None,author_name=None):
     posts = Post.objects.filter(status=1)
     if cat_name:
         posts=Post.objects.filter(category__name=cat_name)
-        
+
     if author_name:
         posts=Post.objects.filter(author__username=author_name)
 
@@ -18,3 +18,11 @@ def blog_single_view(request,pid):
     posts = Post.objects.filter(status=1)
     post=get_object_or_404(posts,pk=pid)
     return render(request,'blog/blog_single.html',context={'post':post})
+
+def blog_search_view(request):
+    posts = Post.objects.filter(status=1)
+    if request.method=='GET':
+        posts=posts.filter(content__contains=request.GET.get('s'))
+    context={'posts':posts}
+    return render(request,'blog/blog_home.html',context)
+    
