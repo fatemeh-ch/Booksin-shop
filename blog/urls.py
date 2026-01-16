ddb49ef35a6afec6683d41512dcb0867308ad302
Fatemeh <fatemeh.ch.official@gmail.com>
2026-01-16 16:48:25 +0330

    Search url was created in urls.py (blog) for searching posts according to their contents.

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ urlpatterns=[
     path('',blog_home_view,name='home'),
     path('single/<int:pid>',blog_single_view,name='single'),
     path('category/<str:cat_name>',blog_home_view,name='category'),
-    path('author/<str:author_name>',blog_home_view,name='author')
+    path('author/<str:author_name>',blog_home_view,name='author'),
+    path('search/',blog_search_view,name='search')
 ]
 
 app_name='blog'
