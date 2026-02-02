6e783207841d0f81e5ab0d06d2f032c249e1a414
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-02 14:08:54 +0330

    Pagination codes were added to blog_home.html.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from blog.models import Post
 # Create your views here.
 
@@ -10,6 +11,18 @@ def blog_home_view(request,cat_name=None,author_name=None):
 
     if author_name:
         posts=Post.objects.filter(author__username=author_name)
+
+    paginator=Paginator(posts,3)
+    try:
+        page_number=request.GET.get('page')
+        posts=paginator.get_page(page_number)
+
+    except EmptyPage:
+        posts=paginator.get_page(1)
+
+    except PageNotAnInteger:
+        posts=paginator.get_page(1)
+
 
     context = {'posts': posts}
     return render(request, 'blog/blog_home.html', context)
