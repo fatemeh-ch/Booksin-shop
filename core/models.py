7d134c3c83ecd689810a9c98b700848a0ad3139e
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-08 00:33:12 +0330

    Models -> Book and Author were created in models.py on app core.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+#Author
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='authors/',
+                              default='authors/default.png')
+
+# Book
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='books/', default='books/default.jpg')
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
