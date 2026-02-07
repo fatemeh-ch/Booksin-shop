f90e476c63065898f9d6ba957e52ffda3257e6a6
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-08 00:07:25 +0330

    Categories of index.html are being loaded from Category Model in Blog app.

--- core/views.py
+++ core/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
+from blog.models import Category
 
 # Create your views here.
 
-
+# Primary page
 def index_view(request):
-    return render(request, 'index.html')
+    
+    categories=Category.objects.all()
+    context={'categories':categories}
+
+    return render(request, 'index.html',context)
+
 
 def contact_view(request):
     return render(request, 'contact.html')
+
 
 def about_view(request):
     return render(request,'about.html')
