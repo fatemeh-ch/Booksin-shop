bed3192f398148bb76d1bbaed5f251a33c6a4a9e
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-18 00:50:50 +0330

    Some tags were added to posts and Tag field was added to admin panel.

--- blog/admin.py
+++ blog/admin.py
@@ -7,7 +7,7 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-Empty'
     list_display = ['title', 'author', 'status', 'created_date']
-    fields = ['title', 'content', 'status', 'author',
+    fields = ['title', 'content', 'status', 'tags','author',
               'counted_views', 'image', 'category', 'published_date']
     list_filter = ('status', 'author')
     ordering = ('-created_date',)
