2fa0b629798db438dfb04ed8fe128dbd6c349cf6
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-18 02:21:44 +0330

    Widget bar responsive codes were modified and Tag section was responsived.

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from .views import *
 
-urlpatterns=[
-    path('',blog_home_view,name='home'),
-    path('single/<int:pid>',blog_single_view,name='single'),
-    path('category/<str:cat_name>',blog_home_view,name='category'),
-    path('author/<str:author_name>',blog_home_view,name='author'),
-    path('tag/<str:tag_name>',blog_home_view,name='tag'),
-    path('search/',blog_search_view,name='search'),
-    
+urlpatterns = [
+    path('', blog_home_view, name='home'),
+    path('single/<int:pid>', blog_single_view, name='single'),
+    path('category/<str:cat_name>', blog_home_view, name='category'),
+    path('author/<str:author_name>', blog_home_view, name='author'),
+    path('tag/<str:tag_name>', blog_home_view, name='tag'),
+    path('search/', blog_search_view, name='search'),
+
 ]
 
-app_name='blog'
+app_name = 'blog'
--- blog/views.py
+++ blog/views.py
@@ -1,56 +1,56 @@
-from django.shortcuts import render,get_object_or_404
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from blog.models import Post
 from taggit.models import Tag
 
 
 # Queries
-tags=Tag.objects.all()
+tags = Tag.objects.all()
 
 
 # Views
 
-def blog_home_view(request,**kwargs):
+def blog_home_view(request, **kwargs):
     posts = Post.objects.filter(status=1)
-    
-    if kwargs.get('cat_name')!=None:
-        posts=Post.objects.filter(category__name=kwargs['cat_name'])
 
-    if kwargs.get('author_name')!=None:
-        posts=Post.objects.filter(author__username=kwargs['author'])
+    if kwargs.get('cat_name') != None:
+        posts = Post.objects.filter(category__name=kwargs['cat_name'])
 
-    if kwargs.get('tag_name')!=None:
-        posts=Post.objects.filter(tags__name__in=[kwargs['tag_name']])
+    if kwargs.get('author_name') != None:
+        posts = Post.objects.filter(author__username=kwargs['author'])
 
-    paginator=Paginator(posts,3)
-    
+    if kwargs.get('tag_name') != None:
+        posts = Post.objects.filter(tags__name__in=[kwargs['tag_name']])
+
+    paginator = Paginator(posts, 3)
+
     try:
-        page_number=request.GET.get('page')
-        posts=paginator.get_page(page_number)
+        page_number = request.GET.get('page')
+        posts = paginator.get_page(page_number)
 
     except EmptyPage:
-        posts=paginator.get_page(1)
+        posts = paginator.get_page(1)
 
     except PageNotAnInteger:
-        posts=paginator.get_page(1)
+        posts = paginator.get_page(1)
 
-
-    context = {'posts': posts,'tags':tags}
+    context = {'posts': posts, 'tags': tags}
     return render(request, 'blog/blog_home.html', context)
 
-def blog_single_view(request,pid):
+
+def blog_single_view(request, pid):
     posts = Post.objects.filter(status=1)
-    post=get_object_or_404(posts,pk=pid)
+    post = get_object_or_404(posts, pk=pid)
 
-    tags=post.tags.all()
+    tags = post.tags.all()
 
-    context={'post':post,'tags':tags}
-    return render(request,'blog/blog_single.html',context)
+    context = {'post': post, 'tags': tags}
+    return render(request, 'blog/blog_single.html', context)
+
 
 def blog_search_view(request):
     posts = Post.objects.filter(status=1)
-    if request.method=='GET':
-        posts=posts.filter(content__contains=request.GET.get('s'))
-    context={'posts':posts}
-    return render(request,'blog/blog_home.html',context)
-    
+    if request.method == 'GET':
+        posts = posts.filter(content__contains=request.GET.get('s'))
+    context = {'posts': posts}
+    return render(request, 'blog/blog_home.html', context)
