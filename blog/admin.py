719857f8b0828e392b13e6f3e35cf246d495c3d5
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-19 15:57:25 +0330

    Comment Model was created in models.py of app -blog- and its manager (CommentAdmin) was created in admin.py (blog).

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -8,12 +8,12 @@ class PostAdmin(SummernoteModelAdmin):
     date_hierarchy = 'created_date'
     empty_value_display = '-Empty'
     list_display = ['title', 'author', 'status', 'created_date']
-    fields = ['title', 'content', 'status', 'tags','author',
+    fields = ['title', 'content', 'status', 'tags', 'author',
               'counted_views', 'image', 'category', 'published_date']
     list_filter = ('status', 'author')
     ordering = ('-created_date',)
     search_fields = ['title']
-    summernote_fields=['content',]
+    summernote_fields = ['content',]
 
 
 # Category manager
@@ -25,6 +25,17 @@ class CategoryAdmin(admin.ModelAdmin):
     empty_value_display = '-Empty'
 
 
+# Comment Manager
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    list_display = ['name', 'subject', 'post', 'approved']
+    empty_display_value = 'empty'
+    list_filter = ['approved']
+    search_fields = ['post', 'email', 'name']
+
+
+
 # Registering models
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,12 @@ from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
 
 
-
 # Category
 class Category(models.Model):
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='categories/',
                               default='categories/default.jpg')
-    
+
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
 
@@ -29,10 +28,28 @@ class Post(models.Model):
     image = models.ImageField(upload_to='blog/', default='blog/default.jpg')
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=0)
-    tags=TaggableManager()
+    tags = TaggableManager()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.title[:20]
+
+    class Meta:
+        ordering = ['-created_date']
+
+
+# Comment
+class Comment(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-created_date']
