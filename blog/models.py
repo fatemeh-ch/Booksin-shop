9e6695547619e8910cc1385ebfcc4a9a46fca609
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-20 23:51:37 +0330

    Sitemaps.py was created for app -blog-.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from django.urls import reverse
 
 
 # Category
@@ -35,6 +36,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title[:20]
+
+    def get_absolute_url(self):
+        return reverse('blog:single', kwargs={'pid': self.id})
 
     class Meta:
         ordering = ['-created_date']
--- booksin/urls.py
+++ booksin/urls.py
@@ -20,10 +20,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.sitemaps.views import sitemap
 from core.sitemaps import StaticViewSitemap
-
+from blog.sitemaps import BlogSitemap
 
 sitemaps = {
-    'static': StaticViewSitemap
+    'static': StaticViewSitemap,
+    'blog':BlogSitemap
 }
 
 
