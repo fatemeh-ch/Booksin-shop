5d1dfedcee18a2f715b6f52c5189b4b34b7f8610
Fatemeh <fatemeh.ch.official@gmail.com>
2026-02-22 11:25:18 +0330

    Login View was completed in views.py app -accounts-.Now if a user is declared in DB and enter their username and password he | she can log in by Login Form and their name will be displayed in home page.

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(
+                    request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+        form = AuthenticationForm()
+        context = {'form': form}
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('/')
 
 
 def register_view(request):
